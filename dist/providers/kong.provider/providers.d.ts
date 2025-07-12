ba0f3c1be6882527c7688bf9dd56a9068bc3360b
Pavel Tarlev <paveltarlev85@gmail.com>
2025-07-13 01:00:22 +0300

    Include compiled dist for GitHub package install

---
+++ dist/providers/kong.provider/providers.d.ts
@@ -0,0 +1,7 @@
+export declare class KongProviders {
+    static GATEWAY_NAME: string;
+    static GATEWAY_PORT: string;
+    static SERVICE_NAME: string;
+    static AUTH_PORT: number;
+    static testString: string;
+}
